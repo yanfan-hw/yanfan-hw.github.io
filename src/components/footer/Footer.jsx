6c6f2de2fdead9f912978f58f487eee17b4ad4c8
YanFan Hw <yanfanhw@gmail.com>
2022-03-27 04:37:06 +0700

    Contact component and Deploy Gh-pages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
 
+import { BsLinkedin, BsGithub, BsFacebook } from "react-icons/bs"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="/" className="footer__logo">Home</a>
+      <ul className='permalinks'>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+      <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <BsLinkedin />
+        </a>
+        <a href="https://github.com/yanfan-hw" rel="noopener noreferrer" target="_blank">
+            <BsGithub />
+        </a>
+        <a href="https://facebook.com/hwng.huynhhai" rel="noopener noreferrer" target="_blank">
+            <BsFacebook />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <strong>&copy; YanFan Hw. Make with love!</strong>
+      </div>
+    </footer>
   )
 }
 
