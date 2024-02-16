b17228c414e1674653d19ccb2d6af0bed3ffd0a4
YanFan Hw <yanfanhw@gmail.com>
2024-02-16 16:10:13 +0700

    [UPDATE] - feature button pre/next for slide testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,49 +1,53 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react"
+import "./testimonials.css"
 
-import { initialData } from '../testimonials/dataTestimonials'
+import { initialData } from "../testimonials/dataTestimonials"
 
 // import Swiper core and required modules
-import { Pagination, Autoplay } from 'swiper';
+import { Pagination, Autoplay, Navigation } from "swiper"
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react"
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
 
 const Testimonials = () => {
-  return (
-    <section id='testimonials'>
-      <h5>Review from Clients</h5>
-      <h2>Testimonials</h2>
+    return (
+        <section id="testimonials">
+            <h5>Review from Clients</h5>
+            <h2>Testimonials</h2>
 
-      <Swiper className="container testimonials__container"
-        // install Swiper modules
-        modules={[Pagination, Autoplay]}
-        spaceBetween={40}
-        slidesPerView={1}
-        autoplay={{
-          delay: 3500,
-          disableOnInteraction: false,
-        }}
-        pagination={{ clickable: true }}
-      >
-        {initialData.data.map(({ avatar, name, review }, index) => {
-          return (
-            <SwiperSlide key={index} className="testimonial">
-              <div className="client__avatar">
-                <img src={avatar} alt={name} />
-              </div>
-              <h5 className='client__name'>{name}</h5>
-              <small className='client__review'>{review}</small>
-            </SwiperSlide>
-          )
-        })
-        }
-      </Swiper>
-    </section>
-  )
+            <Swiper
+                className="container testimonials__container"
+                navigation={true}
+                pagination={{
+                    clickable: true,
+                }}
+                // install Swiper modules
+                modules={[Pagination, Autoplay, Navigation]}
+                spaceBetween={40}
+                slidesPerView={1}
+                autoplay={{
+                    delay: 3500,
+                    disableOnInteraction: false,
+                }}
+            >
+                {initialData.data.map(({ avatar, name, review }, index) => {
+                    return (
+                        <SwiperSlide key={index} className="testimonial">
+                            <div className="client__avatar">
+                                <img src={avatar} alt={name} />
+                            </div>
+                            <h5 className="client__name">{name}</h5>
+                            <small className="client__review">{review}</small>
+                        </SwiperSlide>
+                    )
+                })}
+            </Swiper>
+        </section>
+    )
 }
 
 export default Testimonials
